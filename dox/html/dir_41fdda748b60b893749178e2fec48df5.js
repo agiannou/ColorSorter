3837e0fa3ee2d8427adc7aa8b1c5f8e7c36a83ad
agiannou <74585558+agiannou@users.noreply.github.com>
2020-12-05 16:52:10 -0800

    dox remove to update

--- dox/html/dir_41fdda748b60b893749178e2fec48df5.js
+++
@@ -1,4 +0,0 @@
-var dir_41fdda748b60b893749178e2fec48df5 =
-[
-    [ "main.cpp", "main_8cpp.html", "main_8cpp" ]
-];
--- dox/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['solenoid_2',['solenoid',['../main_8cpp.html#aff93ffa38625663c9f25403faec36858',1,'main.cpp']]],
-  ['steppermotor_3',['steppermotor',['../main_8cpp.html#a44766bb45fccc1d696bb254833340d8b',1,'main.cpp']]]
-];
